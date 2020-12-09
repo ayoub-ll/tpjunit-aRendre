c83dbb29f52a22ec435249dc11fda220d326e7d7
Valentin Stockman <valentin@smartnsoft.com>
2020-12-09 14:41:37 +0100

    Add squeleton for Calculator class

--- src/main/java/src/Calculator.java
+++ src/main/java/src/Calculator.java
@@ -1,5 +1,17 @@
 package src;
 
-public class Calculator {
+import java.util.ArrayList; 
 
+public class Calculator {
+		public ArrayList<Double> CalculateSquareRoots(int firstRange, int secondRange) throws Exception {
+			ArrayList<Double> squareRootsArrayList = new ArrayList<Double>();
+			if(firstRange < secondRange) {
+				for(int i =0; i < secondRange - firstRange; i++) {
+					squareRootsArrayList.add(Math.sqrt(firstRange) + i);
+				}
+			} else {
+				throw new Exception("First parameter must be lower than second parameter !");
+			}
+			return squareRootsArrayList;
+		}
 }
