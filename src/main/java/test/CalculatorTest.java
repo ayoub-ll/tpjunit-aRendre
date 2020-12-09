2092d7f935ad9238d361c9f0177f108dd21a8d96
Ayoub LAAROBI <ayoub.laarobi@gmail.com>
2020-12-09 14:44:54 +0100

    add CalculateSquareRootsTest

--- src/main/java/test/CalculatorTest.java
+++ src/main/java/test/CalculatorTest.java
@@ -1,5 +1,31 @@
 package test;
 
-public class CalculatorTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import src.Calculator;
+
+public class CalculatorTest {
+	private Calculator calculator = new Calculator();
+	
+	@Test
+	public void CalculateSquareRootsTest() {
+		ArrayList<Double> expected = new ArrayList<Double>();
+		
+		expected.add((double) 1);
+		expected.add((double) 2.41);
+		expected.add((double) 1.73);
+		expected.add((double) 2);
+		expected.add((double) 2.24);
+		
+		
+		ArrayList<Double> result = calculator.CalculateSquareRoots(1,5);
+		
+		assertEquals(expected, result);
+	}
 }
